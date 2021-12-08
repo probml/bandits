0129235aa2fc3e1b4130152d884296bc4f892ebc
Gerardo Durán Martín <g.duran@me.com>
2021-12-08 16:40:04 +0000

    fix: run_experiments.py

--- bandits/experiments/run_experiments.py
+++ bandits/experiments/run_experiments.py
@@ -25,7 +25,7 @@ def main():
     experiments_name = ["tabular", "mnist", "movielens", "tabular_subspace"]
 
     for experiment_run, experiment_name in zip(experiments, experiments_name):
-        filename = f"./results{experiment_name}_results_{timestamp}.csv"
+        filename = f"./results/{experiment_name}_results_{timestamp}.csv"
         config = make_config(filename)
         experiment_run.main(config)
     
