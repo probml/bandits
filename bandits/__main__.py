ceecedb2d48bf5ecf2ba41812e88c03a769df0f1
Gerardo Durán Martín <g.duran@me.com>
2021-12-09 14:33:10 +0000

    feat: __main__.py
    Add test function

--- bandits/__main__.py
+++ bandits/__main__.py
@@ -1,9 +1,13 @@
 import fire
 from experiments import plot_results
 from experiments import run_experiments
+from experiments import tabular_test
 
 
 class Experiments:
+    def test(self):
+        tabular_test.main()
+
     def plot_experiments(self):
         plot_results.main()
     
