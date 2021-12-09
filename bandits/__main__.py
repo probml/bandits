28e15f5d3f2621d46ad16f84d45367a3b301533b
Gerardo Durán Martín <g.duran@me.com>
2021-12-09 12:04:22 +0000

    feat: __main__.py
    rename functions; add run_and_plot function

--- bandits/__main__.py
+++ bandits/__main__.py
@@ -4,11 +4,15 @@ from experiments import run_experiments
 
 
 class Experiments:
-    def plot(self):
+    def plot_experiments(self):
         plot_results.main()
     
-    def run(self):
+    def run_experiments(self):
         run_experiments.main()
+    
+    def run_and_plot(self):
+        self.run_experiments()
+        self.plot_experiments()
 
 if __name__ == "__main__":
     fire.Fire(Experiments)
