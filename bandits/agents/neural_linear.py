b54ce6d8f0f87d6f2c5acf2bf3f7f702ed6ce193
Gerardo Duran-Martin <g.duran@me.com>
2023-05-12 14:57:54 +0000

    refactor: neural_linear.py

--- bandits/agents/neural_linear.py
+++ bandits/agents/neural_linear.py
@@ -1,14 +1,10 @@
+import optax
 import jax.numpy as jnp
 from jax import lax
 from jax.random import split
-from jax.ops import index_update
-
-import optax
-
 from flax.training import train_state
 
 from .agent_utils import train
-from scripts.training_utils import MLP
 from tensorflow_probability.substrates import jax as tfp
 
 tfd = tfp.distributions
@@ -20,28 +16,24 @@ class NeuralLinearBandit:
         self.num_features = num_features
         self.num_arms = num_arms
 
-        if model is None:
-            self.model = MLP(500, num_arms)
-        else:
-            try:
-                self.model = model()
-            except:
-                self.model = model
         self.opt = opt
         self.eta = eta
         self.lmbda = lmbda
         self.update_step_mod = update_step_mod
         self.batch_size = batch_size
         self.nepochs = nepochs
+        self.model = model
 
     def init_bel(self, key, contexts, states, actions, rewards):
         key, mykey = split(key)
-        initial_params = self.model.init(mykey, jnp.zeros((self.num_features,)))
+        xdummy = jnp.zeros((self.num_features))
+        initial_params = self.model.init(mykey, xdummy)
         initial_train_state = train_state.TrainState.create(apply_fn=self.model.apply, params=initial_params,
                                                             tx=self.opt)
 
-        mu = jnp.zeros((self.num_arms, 500))
-        Sigma = 1 / self.lmbda * jnp.eye(500) * jnp.ones((self.num_arms, 1, 1))
+        n_hidden_last = self.model.apply(initial_params, xdummy, capture_intermediates=True)[1]["intermediates"]["last_layer"]["__call__"][0].shape[0]
+        mu = jnp.zeros((self.num_arms, n_hidden_last))
+        Sigma = 1 / self.lmbda * jnp.eye(n_hidden_last) * jnp.ones((self.num_arms, 1, 1))
         a = self.eta * jnp.ones((self.num_arms,))
         b = self.eta * jnp.ones((self.num_arms,))
         t = 0
@@ -50,8 +42,10 @@ class NeuralLinearBandit:
             context, action, reward = x
             return self.update_bel(bel, context, action, reward), None
 
+        self.contexts = contexts
+        self.states = states
+
         initial_bel = (mu, Sigma, a, b, initial_train_state, t)
-        self.init_contexts_and_states(contexts, states)
         bel, _ = lax.scan(update, initial_bel, (contexts, actions, rewards))
         return bel
 
@@ -60,24 +54,21 @@ class NeuralLinearBandit:
         Phi, *_ = inter["intermediates"][feature_layer]["__call__"]
         return Phi.squeeze()
 
+
     def cond_update_params(self, t):
         return (t % self.update_step_mod) == 0
-
-    def init_contexts_and_states(self, contexts, states):
-        self.contexts = contexts
-        self.states = states
-
+    
     def update_bel(self, bel, context, action, reward):
         mu, Sigma, a, b, state, t = bel
 
         sgd_params = (state, t)
 
-        def loss_fn(self, params):
+        def loss_fn(params):
             n_samples, *_ = self.contexts.shape
             final_t = lax.cond(t == 0, lambda t: n_samples, lambda t: t.astype(int), t)
             sample_range = (jnp.arange(n_samples) <= t)[:, None]
 
-            pred_reward = self.model.apply({"params": params}, self.contexts)
+            pred_reward = self.model.apply(params, self.contexts)
             loss = (optax.l2_loss(pred_reward, self.states) * sample_range).sum() / final_t
             return loss, pred_reward
 
@@ -101,10 +92,10 @@ class NeuralLinearBandit:
         b_update = b_k + (reward ** 2 + mu_k.T @ Lambda_k @ mu_k - mu_update.T @ Lambda_update @ mu_update) / 2
 
         # update only the chosen action at time t
-        mu = index_update(mu, action, mu_update)
-        Sigma = index_update(Sigma, action, Sigma_update)
-        a = index_update(a, action, a_update)
-        b = index_update(b, action, b_update)
+        mu = mu.at[action].set(mu_update)
+        Sigma = Sigma.at[action].set(Sigma_update)
+        a = a.at[action].set(a_update)
+        b = b.at[action].set(b_update)
         t = t + 1
 
         bel = (mu, Sigma, a, b, state, t)
