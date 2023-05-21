c8f459fa067ea0bdd68464d0d63352977b58527e
Gerardo Duran-Martin <g.duran@me.com>
2023-05-21 08:13:15 +0000

    feat: training.py
    Define run_bandit_trials_multiple()

--- bandits/training.py
+++ bandits/training.py
@@ -2,6 +2,17 @@ import jax
 import jax.numpy as jnp
 from functools import partial
 
+
+def reshape_vvmap(x):
+    """
+    Reshape a 2D array to a 1D array.
+    This is taken to be the output of a double vmap or pmap.
+    """
+    shape_orig = x.shape
+    shape_new = (shape_orig[0] * shape_orig[1], *shape_orig[2:]) 
+    return jnp.reshape(x, shape_new)
+
+
 def step(bel, t, key_base, bandit, env):
     key = jax.random.fold_in(key_base, t)
     context = env.get_context(t)
@@ -51,4 +62,21 @@ def run_bandit_trials_pmap(key, bel, bandit, env, t_start=0, n_trials=1):
     run_partial = jax.pmap(run_partial)
 
     bel, hist = run_partial(keys)
+    return bel, hist
+
+
+def run_bandit_trials_multiple(key, bel, bandit, env, t_start, n_trials):
+    """
+    Run vmap over multiple trials, and pmap over multiple devices
+    """
+    ndevices = jax.local_device_count()
+    nsamples_per_device = n_trials // ndevices
+    keys = jax.random.split(key, ndevices)
+    run_partial = partial(run_bandit_trials, bel=bel, bandit=bandit, env=env, t_start=t_start, n_trials=nsamples_per_device)
+    run_partial = jax.pmap(run_partial)
+
+    bel, hist = run_partial(keys)
+    hist = jax.tree_map(reshape_vvmap, hist)
+    bel = jax.tree_map(reshape_vvmap, bel)
+
     return bel, hist
