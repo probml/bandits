56a37bdb373d4a42550c21b3d9eb836480e1d0bd
Gerardo Duran-Martin <g.duran@me.com>
2023-05-27 06:24:10 +0000

    style: low_rank_filter_bandit

--- bandits/agents/low_rank_filter_bandit.py
+++ bandits/agents/low_rank_filter_bandit.py
@@ -11,7 +11,7 @@ tfd = tfp.distributions
 class LowRankFilterBandit(BanditAgent):
     """
     Regression bandit with low-rank filter.
-    We consider a single neural network with k 
+    We consider a single neural network with k
     outputs corresponding to the k arms.
     """
     def __init__(self, num_features, num_arms, model, memory_size, emission_covariance,
@@ -25,7 +25,7 @@ class LowRankFilterBandit(BanditAgent):
         self.initial_covariance = initial_covariance
         self.dynamics_weights = dynamics_weights
         self.dynamics_covariance = dynamics_covariance
-    
+
     def init_bel(self, key, contexts, states, actions, rewards):
         _, dim_in = contexts.shape
         params = self.model.init(key, jnp.ones((1, dim_in)))
@@ -34,10 +34,10 @@ class LowRankFilterBandit(BanditAgent):
         def apply_fn(flat_params, xs):
             context, action = xs
             return self.model.apply(recfn(flat_params), context)[action, None]
-        
+
         def predict_rewards(flat_params, context):
             return self.model.apply(recfn(flat_params), context)
-        
+
         agent = lofi.RebayesLoFiDiagonal(
             dynamics_weights=self.dynamics_weights,
             dynamics_covariance=self.dynamics_covariance,
@@ -54,11 +54,11 @@ class LowRankFilterBandit(BanditAgent):
         self.predict_rewards = predict_rewards
 
         return bel
-    
+
     def sample_params(self, key, bel):
         params_samp = self.agent.sample_state(bel, key, 1).ravel()
         return params_samp
-    
+
     def update_bel(self, bel, context, action, reward):
         xs = (context, action)
         bel = self.agent.update_state(bel, xs, reward)
@@ -74,7 +74,7 @@ class LowRankGreedy(LowRankFilterBandit):
         super().__init__(num_features, num_arms, model, memory_size, emission_covariance,
                 initial_covariance, dynamics_weights, dynamics_covariance)
         self.epsilon = epsilon
-    
+
     def choose_action(self, key, bel, context):
         key, key_action = jax.random.split(key)
         greedy = jax.random.bernoulli(key, 1 - self.epsilon)
@@ -84,7 +84,7 @@ class LowRankGreedy(LowRankFilterBandit):
         else:
             action = jax.random.randint(key_action, (1,), 0, self.num_arms)
         return action
-    
+
 
     def choose_action(self, key, bel, context):
         key, key_action = jax.random.split(key)
@@ -93,12 +93,12 @@ class LowRankGreedy(LowRankFilterBandit):
         def explore():
             action = jax.random.randint(key_action, shape=(), minval=0, maxval=self.num_arms)
             return action
-        
+
         def exploit():
             params = bel.mean
-            predicted_rewards = self.predict_rewards(params, context) 
+            predicted_rewards = self.predict_rewards(params, context)
             action = predicted_rewards.argmax(axis=-1)
             return action
-        
+
         action = jax.lax.cond(greedy == 1, exploit, explore)
         return action
